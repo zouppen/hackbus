1c7157db090698b31649032885d143e02849717d
Joel Lehtonen <joel.lehtonen@iki.fi>
2017-12-18 21:35:18 +0200

    Loop through two relay boards

--- SConstruct
+++ SConstruct
@@ -5,6 +5,6 @@ conf = pkgconfig.getConf()
 pkgconfig.ensure(conf,'libmodbus >= 3','libmodbus-dev')
 env = conf.Finish()
 
-env.Append(CCFLAGS = "-Werror -O0 -g -Wall -std=gnu99")
+env.Append(CCFLAGS = "-Wall -O0 -g -Wall -std=gnu99")
 env.ParseConfig('pkg-config --cflags --libs libmodbus')
 env.Program('modclient',Glob('src/*.c'))
--- src/main.c
+++ src/main.c
@@ -1,20 +1,57 @@
 // -*- mode: c; c-file-style: "linux" -*-
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <modbus.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+bool relay_control(modbus_t *mb, int slave, int relay, bool state);
+
+bool relay_control(modbus_t *mb, int slave, int relay, bool state)
+{
+	int value = state ? 0x0100 : 0x0200;
+	if (modbus_set_slave(mb, slave) == -1) return false;
+	if (modbus_connect(mb) == -1) return false;
+	if (modbus_write_register(mb, relay, value) == -1) return false;
+	return true;
+}
+	
 
 int main(int argc, char **argv)
 {
 	modbus_t *mb;
-	uint16_t tab_reg[32];
+	int ret = 0;
+	//uint16_t tab_reg[32];
+
+	mb = modbus_new_rtu(argv[1], 9600, 'N', 8, 1);
+	if (mb == NULL) {
+		fprintf(stderr, "Unable to create the libmodbus context\n");
+		ret = -1;
+		goto end;
+	}
+
+	bool state = false;
+	while (true) {
+		state = !state;
+		for (int board = 1; board <= 2; board++) {
+			for (int relay = 1; relay <= 4; relay++) {
+				printf("%d %d %d\n", state, board, relay);
+				bool ok = relay_control(mb, board, relay, state);
+				if (!ok) {
+					fprintf(stderr, "Control failed: %s\n", modbus_strerror(errno));
+					ret = -1;
+					goto free;
+				}
+				sleep(1);
+			}
+		}
+	}
 	
-	mb = modbus_new_tcp("127.0.0.1", 1502);
-	modbus_connect(mb);
-	
-	/* Read 5 registers from the address 0 */
-	modbus_read_registers(mb, 0, 5, tab_reg);
-	
+free:
 	modbus_close(mb);
 	modbus_free(mb);
-	
-	//modbus_t *modbus_new_rtu(const char *device, int baud, char parity, int data_bit, int stop_bit);
+end:
+	return ret;
 }
