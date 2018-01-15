ec384997be34c0ab62ab053a4f9f69e8a2ed36dc
Joel Lehtonen <joel.lehtonen@iki.fi>
2018-01-16 00:51:40 +0200

    Example bus with ordinary switches, toggle switches, and IO actions

---
+++ examples/HacklabDemo.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE RecordWildCards #-}
+module Main where
+
+import Control.Concurrent.STM
+import System.Hardware.Modbus
+import System.Hardware.Modbus.Abstractions
+
+main = do
+  -- Start Modbus master
+  h <- newRTU "/dev/ttyUSB0" 9600 ParityNone 8 1
+  connect h
+  master <- runMaster h
+  -- Get input banks from both rooms
+  (inputKerho,_) <- poll $ readInputBits master 1 0 8
+  (inputPaja,_)  <- poll $ readInputBits master 2 0 8
+  -- Our switch variables, initially all OFF
+  kerhoValot    <- newTVarIO False
+  kerhoLaitteet <- newTVarIO False
+  pajaValot     <- newTVarIO False
+  pajaLaitteet  <- newTVarIO False
+  -- Connect variables to given relays
+  relayControl (readTVar kerhoValot)    (writeBit master 1 0)
+  relayControl (readTVar kerhoLaitteet) (writeBit master 1 1)
+  relayControl (readTVar pajaValot)     (writeBit master 1 2)
+  relayControl (readTVar pajaLaitteet)  (writeBit master 1 3)
+  -- Toggle states with Modbus input
+  toggleButton (inputKerho `item` 0) kerhoValot
+  toggleButton (inputKerho `item` 1) kerhoLaitteet
+  toggleButton (inputPaja  `item` 0) pajaValot
+  toggleButton (inputPaja  `item` 1) pajaLaitteet
+  -- Introduce a panic switch which turns off everything
+  pushButton (inputKerho `item` 3) nop $ atomically $ do
+    writeTVar kerhoValot    False
+    writeTVar kerhoLaitteet False
+    writeTVar pajaValot     False
+    writeTVar pajaLaitteet  False
+  -- Report when club evening starts and ends
+  pushButton (inputKerho `item` 2)
+    (putStrLn "Kerhoilta loppui")
+    (putStrLn "Kerhoilta alkoi")
+  sleepForever
