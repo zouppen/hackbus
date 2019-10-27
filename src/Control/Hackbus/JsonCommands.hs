2b81d0cb39f1efff47ec6914b2796e6cdefcee34
Joel Lehtonen <joel.lehtonen@iki.fi>
2019-10-28 01:24:40 +0200

    Change JSON format to kwak like JSON RPC (but not yet compliant)

--- src/Control/Hackbus/JsonCommands.hs
+++ src/Control/Hackbus/JsonCommands.hs
@@ -3,6 +3,7 @@ module Control.Hackbus.JsonCommands where
 
 import Data.Aeson
 import qualified Data.Text as T
+import qualified Data.Map.Lazy as M
 
 data Command = Read T.Text | Write T.Text Value deriving (Show)
 
@@ -10,12 +11,15 @@ data Answer = Wrote | Return Value | Failed String deriving (Show)
 
 instance FromJSON Command where
   parseJSON = withObject "Command" $ \v -> do
-    r <- v .:? "r"
-    w <- v .:? "w"
-    case (r,w) of
-      (Just x, Nothing) -> pure $ Read x
-      (Nothing, Just x) -> Write x <$> v .: "v"
-      _                 -> fail "Both r and w defined"
+    method  <- v .: "method"
+    case method of
+      "r" -> Read <$> v .: "params"
+      "w" -> do
+        params <- v .: "params"
+        case M.toList params of
+          [(k,v)] -> return $ Write k v
+          _ -> fail "Multiple values not supported yet"
+      x -> fail $ "Unknown command: " ++ x
 
 instance ToJSON Answer where
   toJSON Wrote      = object []
