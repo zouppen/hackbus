ef4735344d0598830ec4bdff76f6991f6ecd6901
Joel Lehtonen <joel.lehtonen@iki.fi>
2019-10-28 02:44:19 +0200

    Bump version, allow skipping list in case of a singleton value

--- src/Control/Hackbus/JsonCommands.hs
+++ src/Control/Hackbus/JsonCommands.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Control.Hackbus.JsonCommands where
 
+import Control.Applicative
+import Control.Monad (liftM)
 import Data.Aeson
 import qualified Data.Text as T
 import qualified Data.Map.Lazy as M
@@ -11,11 +13,12 @@ data Answer = Wrote | Return (M.Map T.Text Value) | Failed String deriving (Show
 
 instance FromJSON Command where
   parseJSON = withObject "Commands" $ \v -> do
-    method  <- v .: "method"
+    method <- v .: "method"
     case method of
-      "r" -> Read <$> v .: "params"
+      "r" -> Read <$> (v .: "params" <|> s (v .: "params"))
       "w" -> Write <$> v .: "params"
       x -> fail $ "Unknown command: " ++ x
+    where s = liftM pure -- In case of single value we don't need a list
 
 instance ToJSON Answer where
   toJSON Wrote      = object []
