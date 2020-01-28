2372421c6ae60825bf1a7d50ad94d5cd9165ff81
Joel Lehtonen <joel.lehtonen@iki.fi>
2020-01-28 03:00:07 +0200

    Allow setting of state externally to wireIO and wireSTM

--- src/Control/Hackbus/ModuleUtils.hs
+++ src/Control/Hackbus/ModuleUtils.hs
@@ -4,28 +4,35 @@ import Control.Concurrent
 import Control.Concurrent.STM
 import Control.Exception (handle, throw)
 import Control.Monad (forever, unless, when, zipWithM_)
-import Control.Monad.Loops (iterateM_)
 
 import Control.Hackbus.Trinary (Input, get)
 
--- |Execute given STM action when another STM action changes state
-wireSTM :: Eq a => Input a -> (a -> STM ()) -> IO ThreadId
-wireSTM source control = forkIO $ flip iterateM_ (const False) $ \same -> atomically $ do
+data Equality a = Const Bool | Equals a
+type StateVar a = TVar (Equality a)
+
+
+stateChanger stateVar source = do
+  oldState <- readTVar stateVar
   state <- get source
-  when (same state) retry
-  -- State has changed (or first iteration)
-  control state
-  -- Return comparator
-  pure (state ==)
+  writeTVar stateVar $ Equals state
+  if isSame oldState state
+    then retry
+    else pure state
+  where isSame old new = case old of
+          Const  x -> x
+          Equals x -> x == new
+
+-- |Execute given STM action when another STM action changes state
+wireSTM :: Eq a => StateVar a -> Input a -> (a -> STM ()) -> IO ThreadId
+wireSTM stateVar source control =
+  forkIO $
+  forever $
+  atomically $
+  stateChanger stateVar source >>= control
 
 -- |Execute given IO action when STM action changes
-wireIO :: Eq a => Input a -> (a -> IO ()) -> IO ThreadId
-wireIO source control = forkIO $ flip iterateM_ (const False) $ \same -> do
-  state <- atomically $ do
-    state <- get source
-    when (same state) retry
-    pure state
-  -- State has changed (or first iteration)
-  control state
-  -- Return comparator
-  pure (state ==)
+wireIO :: Eq a => StateVar a -> Input a -> (a -> IO ()) -> IO ThreadId
+wireIO stateVar source control =
+  forkIO $
+  forever $
+  (atomically $ stateChanger stateVar source) >>= control
