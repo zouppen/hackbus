fdd713d4fe130ea49333b0939852c6d7623c8d49
Joel Lehtonen <joel.lehtonen@iki.fi>
2020-01-31 17:04:55 +0200

    Trinary logic updated to Alternative & Monad

--- src/Control/Hackbus/Trinary.hs
+++ src/Control/Hackbus/Trinary.hs
@@ -1,51 +1,36 @@
 module Control.Hackbus.Trinary where
 
-import Prelude as P
 import Control.Monad
-import Control.Concurrent.STM (STM, atomically, retry)
+import Control.Applicative
 
 -- Kleene logic
 -- https://en.wikipedia.org/wiki/Three-valued_logic
 
-type Input a = STM (Maybe a)
+infixr 3 <&&>
+infixr 2 <||>
 
-infixr 3 ∧
-infixr 3 &&
-infixr 2 ∨
-infixr 2 ||
+optional2 :: (Monad m, Alternative m) => (Maybe a -> Maybe b -> m c) -> m a -> m b -> m c
+optional2 f a b = do
+  ma <- optional a
+  mb <- optional b
+  f ma mb
 
 -- |Trinary AND
-(∧) :: Input Bool -> Input Bool -> Input Bool
-(∧) = liftM2 $ \a b -> case (a, b) of
-  (Just False, _)          -> Just False
-  (_, Just False)          -> Just False
-  (Just True, Just True)   -> Just True
-  _                        -> Just Nothing
+(<&&>) :: (Monad m, Alternative m) => m Bool -> m Bool -> m Bool
+(<&&>) = optional2 $ \a b -> case (a, b) of
+  (Just False, _)          -> pure False
+  (_, Just False)          -> pure False
+  (Just True, Just True)   -> pure True
+  _                        -> empty
 
 -- |Trinary OR
-(∨) :: Input Bool -> Input Bool -> Input Bool
-(∨) = liftM2 $ \a b -> case (a, b) of
+(<||>) :: Maybe Bool -> Maybe Bool -> Maybe Bool
+(<||>) = optional2 $ \a b -> case (a, b) of
   (Just True, _)           -> Just True
   (_, Just True)           -> Just True
   (Just False, Just False) -> Just False
   _                        -> Nothing
 
 -- |Trinary NOT
-not :: Input Bool -> Input Bool
-not = (liftM . liftM) P.not
-
--- |Known value constant
-known :: a -> Input a
-known = (pure . pure)
-
--- |An unknown value constant
-unknown :: Input a
-unknown = pure Nothing
-
--- Aliases
-(&&) = (∧)
-(||) = (∨)
-
--- |Retry STM action until it is no longer Nothing
-get :: Input a -> STM a
-get var = var >>= maybe retry pure
+fnot :: Functor f => f Bool -> f Bool
+fnot = fmap not
