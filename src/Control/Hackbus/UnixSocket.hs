6df09fd6e09d0601816dbf9b9e642ba6825fe7ea
Joel Lehtonen <joel.lehtonen@iki.fi>
2020-04-22 22:09:46 +0300

    Preserve UNIX socket permissions when starting

--- src/Control/Hackbus/UnixSocket.hs
+++ src/Control/Hackbus/UnixSocket.hs
@@ -8,29 +8,38 @@ import qualified Data.ByteString as B (hGetLine)
 import qualified Data.ByteString.Lazy as B
 import Network.Socket
 import System.IO (Handle, IOMode(..), hClose)
-import System.Posix.Files (isSocket, getFileStatus, removeLink)
+import System.Posix.Files
 import System.Directory (doesFileExist)
 
 type LineAction = B.ByteString -> IO B.ByteString 
 
--- |Removes old dangling socket if it exists
-clearDanglingSocket :: FilePath -> IO ()
+-- |Copies file mode and ownership from another file.
+copyPermissions :: FilePath -> FileStatus -> IO ()
+copyPermissions f s = do
+  setFileMode f (fileMode s)
+  setOwnerAndGroup f (fileOwner s) (fileGroup s)
+
+-- |Removes old dangling socket if it exists. Returns its status.
+clearDanglingSocket :: FilePath -> IO (Maybe FileStatus)
 clearDanglingSocket path = do
   exists <- doesFileExist path
-  when exists $ do
-    status <- getFileStatus path
-    if isSocket status
-      then removeLink path
-      else fail "Socket path is a file"
+  if exists
+    then do status <- getFileStatus path
+            if isSocket status
+              then removeLink path >> pure (pure status)
+              else fail "Socket path is a file"
+    else pure Nothing
 
 listenUnixSocket :: (Handle -> IO ()) -> FilePath -> IO ()
-listenUnixSocket handler path = do
-  clearDanglingSocket path
-  bracket open close loop
+listenUnixSocket handler path = bracket open close loop
   where
     open = do
+      mbStatus <- clearDanglingSocket path
       sock <- socket AF_UNIX Stream defaultProtocol
       bind sock $ SockAddrUnix path
+      -- Copy old socket file permissions after the socket has created
+      -- but before starting to accept connections.
+      maybe (pure ()) (copyPermissions path) mbStatus
       listen sock 1024
       return sock
     loop sock = forever $ do
