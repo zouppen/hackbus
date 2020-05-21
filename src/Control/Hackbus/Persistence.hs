000b221b099f66bbdfd499a7a3130a9b731ad992
Joel Lehtonen <joel.lehtonen@iki.fi>
2020-05-22 01:23:41 +0300

    Persistence: Somewhat stable API but implementation not yet saving #2

--- src/Control/Hackbus/Persistence.hs
+++ src/Control/Hackbus/Persistence.hs
@@ -4,22 +4,32 @@ module Control.Hackbus.Persistence where
 import qualified Data.Map.Lazy as M
 import Data.Text (Text, unpack)
 import Data.Aeson
+import Control.Concurrent
 import Control.Concurrent.STM
+import System.Posix.Signals
+import Control.Monad
 
-data PersItem = File Value | Live (STM Value)
+data PersItem = File Value | Live (STM Encoding)
 
 data Persistence = Persistence
   { store :: TVar (M.Map Text PersItem) -- ^Contains all persistent values
   , file  :: FilePath                   -- ^File to store data to
+  , tId   :: ThreadId                   -- ^Thread ID
   }
 
 -- |Load persistence from file.
-newPersistence :: FilePath -> IO Persistence
-newPersistence file = do
+runPersistence :: FilePath -> IO Persistence
+runPersistence file = do
   -- Read JSON or die
   contents <- either error id <$> eitherDecodeFileStrict' file
   -- Create variable and wrap values into dummy STM actions at first
   store <- newTVarIO $ File <$> contents
+  -- Register signal handler
+  quit <- newTVarIO False
+  mainThread <- myThreadId
+  installHandler sigTERM (Catch $ atomically $ writeTVar quit True) Nothing
+  -- Main loop doing all the magic
+  tId <- forkIO $ persLoop file mainThread (readTVar quit) (readTVar store)
   return Persistence{..}
 
 -- |Create new TVar which is backed in persistent storage.
@@ -39,5 +49,20 @@ newTVarPers Persistence{..} name def = do
       Success b -> pure b
     Just (Live _) -> error $ "Persistent value already registered: " ++ unpack name
   var <- newTVar initial
-  writeTVar store $ M.insert name (Live (toJSON <$> readTVar var)) store'
+  writeTVar store $ M.insert name (Live (toEncoding <$> readTVar var)) store'
   return var
+
+persLoop :: FilePath -> ThreadId -> STM Bool -> STM (M.Map Text PersItem) -> IO ()
+persLoop file mainThread quit store = forever $ do
+  time <- registerDelay 10000000 -- 10 sec
+  die <- atomically $ do
+    quit' <- quit
+    time' <- readTVar time
+    case (quit', time') of
+      (True, _) -> pure True
+      (_, True) -> pure False
+      _         -> retry
+  -- Now it's time to write
+  -- TODO
+  -- Stop if it was a signal
+  when die $ killThread mainThread
