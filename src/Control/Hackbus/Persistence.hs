19e3d0b6e3e7c6dd8c4d8f6b638c9beab9ce8ae4
Joel Lehtonen <joel.lehtonen@iki.fi>
2020-05-27 19:53:08 +0300

    Make Persistence a newtype instead of data.

--- src/Control/Hackbus/Persistence.hs
+++ src/Control/Hackbus/Persistence.hs
@@ -17,13 +17,9 @@ import Control.Monad.Loops (iterateUntil)
 
 data PersState = Running | Stopping | Stopped  deriving (Show, Eq)
 
-newtype Map = Map (HashMap Text PersItem)
+newtype Persistence = Persistence (TVar (HashMap Text PersItem))
 
-data PersItem = File Value | Live (STM Value) | Subtree (TVar Map)
-
-data Persistence = Persistence
-  { store :: TVar Map -- ^Contains all persistent values
-  }
+data PersItem = File Value | Live (STM Value) | Subtree Persistence
 
 -- |Wrapping the result as an unparsed value.
 instance FromJSON PersItem where
@@ -44,12 +40,12 @@ withPersistence interval file act = do
     then either error id <$> eitherDecodeFileStrict' file
     else pure M.empty
   -- Create variable and wrap values into dummy STM actions at first
-  store <- newTVarIO $ Map contents
+  pers <- Persistence <$> newTVarIO contents
   -- Start the background task.
   state <- newTVarIO Running
-  forkIO $ persLoop interval file state (readTVar store)
+  forkIO $ persLoop interval file state pers
   -- Run user action until exception is raised.
-  finally (act Persistence{..}) $ do
+  finally (act pers) $ do
     -- Ask the thread to stop.
     putStrLn "Saving state..."
     atomically $ writeTVar state Stopping
@@ -62,18 +58,18 @@ newTVarPers :: (FromJSON a, ToJSON a)
             -> Text         -- ^Key name
             -> a            -- ^Default value if key doesn't exist
             -> STM (TVar a) -- ^New transaction variable
-newTVarPers Persistence{..} name def = do
-  Map store' <- readTVar store
+newTVarPers (Persistence pers) name def = do
+  pers' <- readTVar pers
   -- Finding stored value. Using supplied default if given key doesn't
   -- exist.
-  initial <- case M.lookup name store' of
+  initial <- case M.lookup name pers' of
     Nothing       -> pure def
     Just (File a) -> case fromJSON a of
       Error msg -> fail $ "Persistent value parse error on " ++ unpack name ++ ": " ++ msg
       Success b -> pure b
     _ -> fail $ "Persistent value already registered: " ++ unpack name
   var <- newTVar initial
-  writeTVar store $ Map $ M.insert name (Live (toJSON <$> readTVar var)) store'
+  writeTVar pers $ M.insert name (Live (toJSON <$> readTVar var)) pers'
   return var
 
 -- |Create a new subtree which is a different namespace from the
@@ -83,22 +79,22 @@ newTVarPers Persistence{..} name def = do
 newSubtree :: Persistence     -- ^Persistence object
            -> Text            -- ^Key name
            -> STM Persistence -- ^New subtree
-newSubtree Persistence{..} name = do
-  Map store' <- readTVar store
+newSubtree (Persistence pers) name = do
+  pers' <- readTVar pers
   -- Finding stored value. Using supplied default if given key doesn't
   -- exist.
-  initial <- case M.lookup name store' of
+  initial <- case M.lookup name pers' of
     Nothing       -> pure M.empty
     Just (File a) -> case fromJSON a of
       Error msg -> fail $ "Persistent value parse error on " ++ unpack name ++ ": " ++ msg
       Success b -> pure b
     _ -> fail $ "Persistent value already registered: " ++ unpack name
-  subtree <- newTVar (Map initial)
-  writeTVar store $ Map $ M.insert name (Subtree subtree) store'
+  subtree <- newTVar initial
+  writeTVar pers $ M.insert name (Subtree (Persistence subtree)) pers'
   return $ Persistence subtree
 
-persLoop :: Int -> FilePath -> TVar PersState -> STM Map -> IO ()
-persLoop interval file stateVar store = loop $ do
+persLoop :: Int -> FilePath -> TVar PersState -> Persistence -> IO ()
+persLoop interval file stateVar pers = loop $ do
   timeVar <- registerDelay $ 1000000 * interval
   ret <- atomically $ do
     state <- readTVar stateVar
@@ -106,19 +102,19 @@ persLoop interval file stateVar store = loop $ do
     when (state == Running && not timeout) retry
     pure state
   -- Now it's time to write
-  json <- atomically $ do
-    Map store' <- store
-    traverse itemToValue store'
+  json <- atomically $ itemToValue $ Subtree pers
   encodeFile file json
   pure ret
   where
     loop a = finally (void $ iterateUntil (==Stopping) a) sayQuit
     sayQuit = atomically $ writeTVar stateVar Stopped
 
+-- |Recursively convert given persistent item to JSON.
 itemToValue :: PersItem -> STM Value
 itemToValue item = case item of
   File a -> pure a
   Live a -> a
-  Subtree var -> do
-    Map a <- readTVar var
-    toJSON <$> traverse itemToValue a
+  Subtree (Persistence var) -> do
+    itemMap <- readTVar var
+    valueMap <- traverse itemToValue itemMap
+    pure $ toJSON valueMap
