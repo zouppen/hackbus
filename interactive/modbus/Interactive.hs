5d2c01fb2b2c45552f0e7e077b0e0704e59be54c
Joel Lehtonen <joel.lehtonen@iki.fi>
2020-11-16 15:41:37 +0200

    Configurable baud rate in interactive mode

--- interactive/modbus/Interactive.hs
+++ interactive/modbus/Interactive.hs
@@ -1,9 +1,13 @@
 -- |Quick hack for getting interactive prompt
 module Interactive where
 
+import Data.List
+import Data.Maybe
 import System.Hardware.Modbus
 import System.Hardware.Modbus.LowLevel
 import qualified System.Console.Readline as R
+
+baudrates = ["110", "300", "600", "1200", "2400", "4800", "9600", "14400", "19200", "38400", "57600", "115200", "128000", "256000"]
 
 modbusInit = do
   R.setCompletionEntryFunction (Just R.filenameCompletionFunction)
@@ -11,6 +15,8 @@ modbusInit = do
   device <- case ans of
     Nothing -> fail "Interrupted"
     Just x -> return $ takeWhile (/=' ') x
-  h <- newRTU device 9600 ParityNone 8 1
+  R.setCompletionEntryFunction $ Just $ \input -> pure $ filter (isPrefixOf input) baudrates
+  ans <- R.readline "Baud rate: "
+  h <- newRTU device (read $ fromJust ans) ParityNone 8 1
   connect h
   return h
